47e9aa72dc60a65605175a87c399aeae21b30e64
Pedro Sérgio <www.psro@gmail.com>
2021-01-10 12:01:33 -0300

    hero & layouts updated

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.default')
 
 @section('content')
-    <div class="hero" style="background-image: url('https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80')">
-        
+    <div class="hero p-4" style="background-image: url('https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80')">
+        <div class="shadow bg-white hero-card p-4">
+            <h3>Busque sempre mais</h3>
+            <p>O aprendizado mantém você à frente. Adquira habilidades procuradas que vão deixar todos impressionados.</p>
+        </div>
     </div>
+    <div class="bg-white hero-card-bottom p-4">
+        <h3>Busque sempre mais</h3>
+        <p>O aprendizado mantém você à frente. Adquira habilidades procuradas que vão deixar todos impressionados.</p>
+    </div>
+
 @endsection
--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,14 +1,15 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white border border-0  px-3 p-1 default-nav">
   <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
     <a class="navbar-brand text-primary" href="#">
       <div class="logo">
         <span class="logo-icon">R</span>
         Rsrcy
       </div>
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 align-items-center align-items-none-md">
     
